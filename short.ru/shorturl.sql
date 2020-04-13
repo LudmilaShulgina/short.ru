8323a0b3c9e08569dc0edd1bea0ff6110cb9c844
Ludmila_Shulgina <shulgina@quantnet.ai>
2020-04-13 20:29:39 +0300

    dumb BD

--- short.ru/shorturl.sql
+++ short.ru/shorturl.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1
--- Время создания: Фев 05 2018 г., 22:12
--- Версия сервера: 5.5.25
--- Версия PHP: 5.3.13
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 13 2020 г., 20:27
+-- Версия сервера: 10.3.22-MariaDB-log
+-- Версия PHP: 7.1.33
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База данных: `shorturl`
@@ -26,23 +27,41 @@ SET time_zone = "+00:00";
 -- Структура таблицы `link`
 --
 
-CREATE TABLE IF NOT EXISTS `link` (
-  `link_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `link` (
+  `link_id` int(11) NOT NULL,
   `link_url` text NOT NULL,
-  `hash` varchar(32) NOT NULL,
-  PRIMARY KEY (`link_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=55 ;
+  `hash` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `link`
 --
 
 INSERT INTO `link` (`link_id`, `link_url`, `hash`) VALUES
-(50, 'https://ru.stackoverflow.com/questions/563020/Очистка-текстового-поля-ввода-при-нажатии-кнопки', '50'),
-(51, 'https://javascript.ru/forum/misc/45074-ochistit-value-polya-input-type=text.html', '51'),
-(52, 'https://e.mail.ru/messages/folder/1/', '52'),
-(53, 'https://blog.sk8er.name/rabota/vyravnivanie-bloka-po-centru/', '53'),
-(54, 'https://www.livelib.ru/', '54');
+(86, 'https://moika78.ru/news2/2019/10/kaban-e1571659941318.png', '586967794'),
+(87, 'https://miro.medium.com/max/4096/1*fyQoQZpqXSYgIRKKiYrfww.jpeg', '815245598'),
+(88, 'https://img-tv.vl.ru/fhd/250cecdd41586845b14151825056287019012c.jpg', '1499941333');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `link`
+--
+ALTER TABLE `link`
+  ADD PRIMARY KEY (`link_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `link`
+--
+ALTER TABLE `link`
+  MODIFY `link_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
